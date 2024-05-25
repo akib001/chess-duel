877ab905902ebb178af989f21c68b972cc3741bb
akib <akibahmed2011@gmail.com>
2024-05-25 11:52:44 +0600

    pawn capture logic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const CHESS_PIECES = require("./constants.js");
 
 const board = [];
+const capturedPieces = [];
 
 for (let i = 0; i < 8; i++) {
   board.push([]);
@@ -33,34 +34,46 @@ board[7][5] = CHESS_PIECES.BLACK_BISHOP;
 board[7][6] = CHESS_PIECES.BLACK_KNIGHT;
 board[7][7] = CHESS_PIECES.BLACK_ROOK;
 
-const checkPawnMoves = (startRow, startCol, endRow, endCol, playerType) => {
+const checkPawnMoves = (
+  startRow,
+  startCol,
+  endRow,
+  endCol,
+  playerType,
+  isTryingToCapture = false
+) => {
   const multi = playerType == 0 ? -1 : 1;
   // check outbound
-  if (endRow < 0 || endRow > 7 || endRow < 0 || endCol > 7) {
+  if (endRow < 0 || endRow > 7 || endRow < 0 || endCol > 7 || (!isTryingToCapture && board[endRow][endCol])) {
     console.log("outbound");
     return false;
   }
 
   let rowDiff = playerType * (startRow - endRow);
   let colDiff = Math.abs(startCol - endCol);
-
-  if (colDiff > 0) {
+  if (isTryingToCapture && colDiff == 1 && rowDiff == 1) {
+    return true;
+  } else if (colDiff > 0) {
     console.log("can not go horizontal");
     return false;
   } else {
-    if(rowDiff < 0) {
-        console.log('can not go backward');
-        return false;
+    if (rowDiff < 0) {
+      console.log("can not go backward");
+      return false;
     }
     // 1st time
     if (startRow == 1 || startRow == 6) {
-        if(rowDiff > 2) {
-            return false;
-        }
+      // overlapping another piece
+      if (rowDiff > 2) {
+        return false;
+      } else if(board[row + (multi * 1)][col]) {
+        console.log('another piece exist in the path');
+        return false;
+      }
     } else {
-        if(rowDiff > 1) {
-            return false;
-        }
+      if (rowDiff > 1) {
+        return false;
+      }
     }
   }
 
@@ -69,9 +82,17 @@ const checkPawnMoves = (startRow, startCol, endRow, endCol, playerType) => {
 
 const move = (startRow, startCol, endRow, endCol, playerType) => {
   const targetPiece = board[startRow][startCol];
+  const isTryingToCapture = board[endRow][endCol] && board[endRow][endCol].includes(playerType == 0 ? 'b' : 'w');
 
-  if(!targetPiece) {
-    console.log('could not find the piece');
+  console.log('isTryingToCapture');
+
+  if (!targetPiece) {
+    console.log("could not find the piece");
+    return;
+  }
+
+  if(board[endRow][endCol] && board[endRow][endCol].includes(playerType == 0 ? 'w' : 'b')) {
+    console.log('can not capture own piece');
     return;
   }
 
@@ -80,15 +101,29 @@ const move = (startRow, startCol, endRow, endCol, playerType) => {
     targetPiece ||
     CHESS_PIECES.WHITE_PAWN
   ) {
-    const checkResult = checkPawnMoves(startRow, startCol, endRow, endCol, playerType);
-    console.log('checkResult');
-    if(checkResult) {
-        board[startRow][startCol] = "";
-        board[endRow][endCol] = targetPiece;
-    }
-  }
+    const checkResult = checkPawnMoves(
+      startRow,
+      startCol,
+      endRow,
+      endCol,
+      playerType,
+      isTryingToCapture
+    );
 
-  console.log('board', board);
+    if (checkResult) {
+      if(isTryingToCapture) {
+        let capturedPiece = board[endRow][endCol];
+        capturedPieces.push(capturedPiece);
+        console.log('capturedPiece',capturedPiece)
+      }
+      board[startRow][startCol] = "";
+      board[endRow][endCol] = targetPiece;
+    }
+  };
+
+  console.log('',startCol, endCol, endRow, endCol, 'player type', playerType);
+
+  console.log("board", board);
 };
 
 move(6, 0, 5, 0, 1);
@@ -96,3 +131,6 @@ move(6, 0, 5, 0, 1);
 move(6, 1, 4, 1, 1);
 
 move(1, 0, 3, 0, 0);
+
+move(4, 1, 3, 0, 1);
+
