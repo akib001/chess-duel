d295fb64a1f7b1365971b85b31ceeae5d3a1030b
akib <akibahmed2011@gmail.com>
2024-07-30 02:26:58 +0600

    responsive container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Board from "@/components/board";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
+    <div className="w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1100px] 2xl:max-w-[1300px] mx-auto px-4">
       <Board />
     </div>
   );
